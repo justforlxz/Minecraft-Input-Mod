40b9e1cda8aba4d3ff9f82a9f7d6cf2ac18fed06
kirigaya <kirigaya@mkacg.com>
2017-12-11 17:50:23 +0800

    update

--- src/main/java/com/mkacg/minecraft/input/Main.java
+++ src/main/java/com/mkacg/minecraft/input/Main.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod(modid = Main.MODID, version = Main.VERSION)
 public class Main {
-    public static final String MODID = "mkacgInputMod";
+    public static final String MODID = "mkacginputmod";
     public static final String VERSION = "1.0";
 
     @SideOnly(Side.CLIENT)
